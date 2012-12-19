157fd264af537ba2e979a4214e3922797d56624c
George Shore <george.shore@bigfatsodacompany.com>
2012-12-19 16:00:59 +0000

    Screens: add Intro screen
    Signed-off-by: George Shore <george.shore@bigfatsodacompany.com>

---
+++ space-game/space-game/Screens/Intro.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SpaceGame.Managers;
+
+namespace SpaceGame.Screens
+{
+    public class Intro : Screen
+    {
+        #region Fields & properties
+        private ScreenManager _sm = null;
+        private GameManager _gm = null;
+        #endregion
+
+        public Intro(ScreenManager sm, GameManager gm)
+        {
+            _sm = sm;
+            _gm = gm;
+
+            FadeOut = 0.0f;
+        }
+
+        public override void SetFocus(ContentManager content, bool focus)
+        {
+        }
+
+        public override void ProcessInput(float elapsedTime, InputManager input)
+        {
+            for (int i = 0; i < SystemConfig.MaxPlayers; i++)
+            {
+                if (input.IsKeyPressed(i, Keys.Enter))
+                    _sm.SetNextScreen(ScreenType.ScreenDemo);
+            }
+        }
+
+        public override void Update(float elapsedTime)
+        {
+        }
+
+        public override void Draw3D(GraphicsDevice gd)
+        {
+            if (gd == null)
+                throw new ArgumentNullException("gd");
+
+            gd.Clear(Color.Black);
+        }
+
+        public override void Draw2D(GraphicsDevice gd, FontManager font)
+        {
+        }
+    }
+}
